dd7ac6e6a4d1c4394b4ef5f642dc01d0ede4ef65
Jorge Ejarque <jorge.ejarque@bsc.es>
2022-03-16 07:57:14 +0100

    add basic https security for the service

--- builder_service.py
+++ builder_service.py
@@ -1,21 +1,93 @@
 
 from flask import Flask, request, jsonify, abort, g
 from flask import send_file
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
 import logging
+import time
 
 from config import configuration
 from image_builder.build import ImageBuilder 
 
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = configuration.secret_key
+app.config['SQLALCHEMY_DATABASE_URI'] = configuration.database
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 builder = ImageBuilder(configuration.repositories_cfg, configuration.build_cfg, configuration.registry_cfg)
 
+db = SQLAlchemy(app)
+
+auth = HTTPBasicAuth()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(32), index = True)
+    password_hash = db.Column(db.String(64))
+
+    def hash_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def generate_auth_token(self, expires_in=600):
+        return jwt.encode(
+            {'id': self.id, 'exp': time.time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_auth_token(token):
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'],
+                              algorithms=['HS256'])
+        except:
+            return
+        return User.query.get(data['id'])
+
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    user = User.verify_auth_token(username_or_token)
+    if not user:
+        # try to authenticate with username/password
+        user = User.query.filter_by(username=username_or_token).first()
+        if not user 
+        
+        if not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(404, "Incorrect request beacuse of empty username or password")    
+    if User.query.filter_by(username=username).first() is not None:
+        abort(400, "User already exists.")    
+    user = User(username=username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return (jsonify({'username': user.username}), 201)
+
+@app.route('/login')
+@auth.login_required
+def get_token():
+    token = g.user.generate_auth_token(6000)
+    print("generated token: " + token)
+    return jsonify({ 'token': token, 'duration': 6000 })
 
 
 @app.route('/images/download/<name>')
+@auth.login_required
 def download_image (name):
     #For windows you need to use drive name [ex: F:/Example.pdf]
     path = builder.get_filename(name)
@@ -24,6 +96,7 @@ def download_image (name):
     return send_file(path, as_attachment=True)
 
 @app.route('/build/', methods= ['POST'])
+@auth.login_required
 def build_Image ():
     logging.debug(str(request))
     content = request.json
@@ -44,6 +117,7 @@ def build_Image ():
     
 
 @app.route('/build/<id>', methods=['GET'])
+@auth.login_required
 def check (id):
     try:
         return jsonify(builder.check_build(id))
@@ -51,6 +125,4 @@ def check (id):
         abort(400, "Bad request: " + str(e))
 
 if __name__ == '__main__':
-    #app.run(port=5000,debug=True, ssl_context='adhoc') 
-    app.run(port=5000,debug=True)
-    
+    app.run(port=5000,debug=True, ssl_context='adhoc') 
--- client.py
+++ client.py
@@ -1,29 +1,36 @@
+from curses.ascii import HT
 import requests
+from requests.auth import HTTPBasicAuth
 import time
 ssl_verify = False
-url = "http://localhost:5000"
-
-js_request = {"machine": {"platform": "linux/amd64", "architecture": "skylake", "container_engine": "singularity"},
+url = "https://localhost:5000"
+js_build_request = {"machine": {"platform": "linux/amd64", "architecture": "skylake", "container_engine": "singularity"},
 "workflow":"minimal_workflow",
 "step_id" :"wordcount",
 "force": True}
-
-res = requests.post(url+'/build/', json=js_request, verify=ssl_verify)
+user="test"
+pswd="T3st22"
+#js_register_request = {"username" : "test", "password" : "T3st22"}
+#requests.post(url +'/register', json=js_register_request, verify=ssl_verify)
+auth_u_p = HTTPBasicAuth(user,pswd)
+res = requests.get(url+'/login', verify=ssl_verify,  auth=HTTPBasicAuth(user,pswd))
+token = res.json()['token']
+hed = {'Authorization': 'Bearer ' + token}
+print("Login successfull. Token:" + token)
+auth_u_p = HTTPBasicAuth(token,None)
+res = requests.post(url+'/build/', json=js_build_request, verify=ssl_verify, auth=auth_u_p)
 if res.ok:
     id = res.json()['id']
     print(" Submitted with id" + id)
     status = 'PENDING'
     while (status != 'FAILED') and (status != 'FINISHED'): 
         time.sleep(5)
-        res2 = requests.get(url+'/build/'+id, verify=ssl_verify)
+        res2 = requests.get(url+'/build/'+id, verify=ssl_verify, auth=auth_u_p)
         if res2.ok:
-            ret = res2.json()
-            status = ret['status']
+            status = res2.json()['status']
             print ("Build " + id + ": status - " + status)
-            if status == 'FAILED'  or status == 'FINISHED':
-                print("Result:" + str(ret))
         else:
             print("Error \n"+str(res2))
             status = 'FAILED'
 else :
-    print("Error \n"+str(res))
+    print("Error \n"+str(res)) 
