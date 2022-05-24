9f800b2ff3ceabdb94d1a982bfba9aefac07284f
Jorge Ejarque <jorge.ejarque@bsc.es>
2022-05-24 14:49:41 +0200

    add simple client

---
+++ client.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# request build
+
+user=$1
+passwd=$2
+url=$3
+action=$4
+
+shift 4
+
+if [ "$action" == "build" ]; then
+   request=$1
+   echo "$request"
+   curl -k -L -X POST $url/image_creation/build \
+      -H "Content-Type: application/json" \
+      -d @$request --user "$user:$passwd"
+   #$cmd
+elif [ "$action" == "status" ]; then
+   id=$1
+   curl -k -L -X GET $url/image_creation/build/$id --user "$user:$passwd"
+elif [ "$action" == "download" ]; then
+   image=$1
+   wget --no-check-certificate --user $user --password $passwd $url/image_creation/images/download/$image
+fi
